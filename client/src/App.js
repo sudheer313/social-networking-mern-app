34933a7f080216d11a0b87158c0676c054496cce
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-10-25 17:38:30 +1100

    initial client route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">helloWorld</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default signup
