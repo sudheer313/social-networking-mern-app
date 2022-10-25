52538f7bca47c67e27a7283b05c98352dea23d77
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-10-25 22:23:54 +1100

    login and signup initial setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,59 +1,37 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div >
-    <h1 className="text-center">Login</h1>
-    <div className=" w-full max-w-xs right-5  ">
-        <form class="text-center bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
-          <div class="mb-4">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
-            >
-              Username
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="E-mail"
-              type="text"
-              placeholder="E-mail address"
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="password"
-            >
-              Password
-            </label>
-            <input
-              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-            <p class="text-red-500 text-xs italic">Please choose a password.</p>
-          </div>
-          <div class="flex items-center justify-between">
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
-              Login
-            </button>
-            <a
-              class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
-        <p class="text-center text-gray-500 text-xs">
-          &copy;2020 Acme Corp. All rights reserved.
-        </p>
-      </div>
-    </div>;
+  return (
+    <div className="text-center pt-4 flex flex-col items-center self-auto ">
+      <h1 className="text-3xl pb-4 ">Social Networking App</h1>
+      <h3 className="text-2xl pb-2"> SignIn</h3>
+      <p className="text-gray-500">
+        Already have a Account?
+        <Link to="/login" className="underline text-blue-700">
+          Login
+        </Link>
+      </p>
+
+      <form className="flex flex-col gap-4">
+        <input
+          type="e-mail"
+          placeholder="E-mail *"
+          className="border p-3 rounded-lg w-80"
+        />
+        <input
+          type="password"
+          placeholder="Password *"
+          className="border p-3 rounded-lg w-80"
+        />
+        <button
+          type="submit"
+          className="border p-3 bg-blue-600 text-white w-80"
+        >
+          SIGNUP
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
