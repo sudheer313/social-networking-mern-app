df80a3541543fcc117f4e73e0547c43c4f8f535e
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-10-25 21:00:19 +1100

    update login and Signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="text-center pt-4 justify-items-center">
+      <h1 className="text-3xl pb-4">Social Networking App</h1>
+      <h3 className="text-2xl pb-2"> SignUp</h3>
+      <p className="text-gray-500">
+        Already have a Account?
+        <Link to="/login" className="underline text-blue-700">
+          Login
+        </Link>
+      </p>
+      <div className=" w-full max-w-xs right-5  ">
+        <form class=" bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="User Name"
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              E-mail address
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="E-mail address"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p class="text-red-500 text-xs italic">Please choose a password.</p>
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+          &copy;2020 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default signup
+export default signup;
